86b92ea4a4da9231eaca24f9cda4c2eae17bf323
rai8 <hytonne@outlook.com>
2021-04-09 00:44:39 +0300

    ninja id card project

--- lib/main.dart
+++ lib/main.dart
@@ -2,53 +2,104 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: Home(),
+        home: Card(),
       ),
     );
 
-class Home extends StatelessWidget {
+class Card extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+          backgroundColor: Colors.grey[900],
+
       appBar: AppBar(
-        backgroundColor: Colors.green[300],
-        title: Text('My First App'),
-        centerTitle: true,
+          title: Text('School Card ID'),
+          centerTitle: true,
+          backgroundColor: Colors.grey[850],
+          elevation: 0.0,
       ),
-      body: Row(
-        children: <Widget>[
-          Expanded(child: Image.asset('assets/crystal.jpg')),
-          Expanded(
-                      child: Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.cyan,
-              child: Text('Container 1'),
+      body: Padding(
+        
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/thumb.jpg'),
+                radius: 40.0,
+              ),
             ),
-          ),
-          Expanded(
-                      child: Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.amber,
-              child: Text('Container 2'),
+            Divider(
+              height: 60.0,
+              color: Colors.grey[800]
             ),
-          ),
-          Expanded(
-                      child: Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.pink,
-              child: Text('Container 3'),
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+              ),
             ),
-          ),
-        ],
-          
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print('you clicked me');
-        },
-        backgroundColor: Colors.green[300],
-        child: Text('Click'),
-      ),
+            SizedBox(
+              height: 10.0,
+              ),
+            Text(
+              'Rhytonne Brite',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+              ),
+              Text(
+              'CURRENT NINJA LEVEL',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+              ),
+            Text(
+              '8',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+              ),
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.amberAccent[200],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 0.0),
+                    child: Text(
+                      'hytonne@outlook.com',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        letterSpacing: 1.0,
+                        color: Colors.white
+                      ),
+                    ),
+                  )
+                ],
+              ),
+          ],
+        ),
+        ),
     );
   }
 }
