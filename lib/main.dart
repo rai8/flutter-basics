b043ff738f3c28e10eb27e686ef0147f07eea38e
rai8 <hytonne@outlook.com>
2021-04-08 21:51:02 +0300

    working with images and assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,32 @@
 import 'package:flutter/material.dart';
 
-/* void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Text('Hello there'),
-    );
-  }
-} */
-
 void main() => runApp(
       MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.green[300],
-            title: Text('My First App'),
-            centerTitle: true,
-          ),
-          body: Center(
-            child: Text(
-            'This is the body',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.grey[600],
-              fontFamily: 'IndieFlower'
-              
-            ),
-            ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: (){},
-            backgroundColor: Colors.green[300],
-            child: Text('Click'),
-          ),
-        ),
+        home: Home(),
       ),
     );
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green[300],
+        title: Text('My First App'),
+        centerTitle: true,
+      ),
+      body: Center(
+        //child: Image.network('url') or Image.asset('assets/crystal.jpg)
+        child:Image(
+         // image: NetworkImage('https://images.unsplash.com/photo-1557008075-7f2c5efa4cfd?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8Zm94fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'),
+          image: AssetImage('assets/crystal.jpg'),
+          )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.green[300],
+        child: Text('Click'),
+      ),
+    );
+  }
+}
