f570cca75b525139785ba44fb3b21990826fd418
rai8 <hytonne@outlook.com>
2021-04-09 11:34:51 +0300

    setting up template design

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,7 @@
+import 'package:appulda/pages/home.dart';
 import 'package:flutter/material.dart';
-import 'quote.dart';
-import 'quote_card.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: QuoteList(),
-      ),
-    );
+void main() => runApp(MaterialApp(
+  home: Home()
+));
 
-class QuoteList extends StatefulWidget {
-  @override
-  _QuoteListState createState() => _QuoteListState();
-}
-
-class _QuoteListState extends State<QuoteList> {
-  //defining our variables
-  List<Quote> quotes = [
-    Quote(
-        author: 'Oscar Wilde',
-        text: 'Be yourself; everyone else is already taken'),
-    Quote(
-        author: 'Krayl Michigan',
-        text: 'I have nothing to declare except my genius'),
-    Quote(
-        author: 'Stanley Bailey',
-        text: 'The truth is rarely pure and never simple'),
-  ];
-
-  //create a function that returns a widget for our card template
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text('Awesome Quotes'),
-        centerTitle: true,
-        backgroundColor: Colors.redAccent,
-        elevation: 0.0,
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
-        child: Column(
-          /* children: quotes.map((quote) {
-            return Text(quote);
-          }).toList(), */
-          children: quotes
-              .map((quote) => QuoteCard(
-                  quote: quote,
-                  delete: () {
-                    setState(() {
-                      quotes.remove(quote);
-                    });
-                  }))
-              .toList(),
-        ),
-      ),
-    );
-  }
-}
