f53669835f07b7320851638d9f38bbc49ed0f802
rai8 <hytonne@outlook.com>
2021-04-09 12:00:40 +0300

    mapping and routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
+
+
+
 import 'package:appulda/pages/home.dart';
 import 'package:flutter/material.dart';
+import 'package:appulda/pages/loading.dart';
+import 'package:appulda/pages/choose_location.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home()
+  initialRoute: '/home',
+  routes: {
+    '/': (context)=>Loading(),
+    '/home': (context)=>Home(),
+    '/location': (context)=>ChooseLocation(),
+
+  },
 ));
 
--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -10,6 +10,13 @@ class _ChooseLocationState extends State<ChooseLocation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Choose a Location'),
+        backgroundColor: Colors.blue[900],
+        centerTitle: true,
+        elevation: 0.0,
+      ),
       body: Text('This is the choose location screen'),
       
     );
