5d571e5be81edd171a3b876f279cec15052c3ed2
rai8 <hytonne@outlook.com>
2021-04-09 15:21:28 +0300

    adding loaders and spinners

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,8 @@
 import 'package:appulda/services/world_time.dart';
 import 'package:flutter/material.dart';
 
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
 class Loading extends StatefulWidget {
   @override
   _LoadingState createState() => _LoadingState();
@@ -10,7 +12,7 @@ class _LoadingState extends State<Loading> {
   //await is applied to void which is nt a future
   void setupWorldTime() async {
     WorldTime instance = WorldTime(
-        location: 'Berlin', flag: 'Germany.png', url: 'Europe/Berlin');
+        location: 'Kenya', flag: 'Germany.png', url: 'Africa/Nairobi');
 
     //get time for a given location
     await instance.getTime();
@@ -30,10 +32,14 @@ class _LoadingState extends State<Loading> {
 
   @override
   Widget build(BuildContext context) {
+  
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(50.0),
-        child: Text('Loading...'),
+     backgroundColor: Colors.blue[900],
+      body: Center(
+        child: SpinKitFadingCube(
+          color: Colors.cyan[600] ,
+          size: 140.0,
+          ),
       ),
     );
   }
