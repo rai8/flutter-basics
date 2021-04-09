988b929be5ed63578286f54142431d5423d60e7d
rai8 <hytonne@outlook.com>
2021-04-09 12:31:02 +0300

    asynchronous code

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
 
-
 class ChooseLocation extends StatefulWidget {
   @override
   _ChooseLocationState createState() => _ChooseLocationState();
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  void getData() async {
+    //simulate a network request for a user
+    String username = await Future.delayed(Duration(seconds: 3), () {
+      return 'Yoshi';
+    });
+    //simulate a network request for user biography
+    String bio = await Future.delayed(Duration(seconds: 2), () {
+      return 'vegan, meat pie lover';
+    });
+
+    print('$username - $bio');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +36,6 @@ class _ChooseLocationState extends State<ChooseLocation> {
         elevation: 0.0,
       ),
       body: Text('This is the choose location screen'),
-      
     );
   }
 }
