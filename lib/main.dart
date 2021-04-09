10ef9ad33b318dea20ad04aab7a12d942e56e28c
rai8 <hytonne@outlook.com>
2021-04-09 07:56:19 +0300

    working with lists

--- lib/main.dart
+++ lib/main.dart
@@ -2,113 +2,40 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: Card(),
+        home: QuoteList(),
       ),
     );
 
-class Card extends StatefulWidget {
+class QuoteList extends StatefulWidget {
   @override
-  _CardState createState() => _CardState();
+  _QuoteListState createState() => _QuoteListState();
 }
 
-class _CardState extends State<Card> {
-  //define our variables here
-  int ninjaLevel = 1;
+class _QuoteListState extends State<QuoteList> {
+  //defining our variables
+  List<String> quotes = [
+    'Be yoursel , everyone else is already taken',
+    'I have nothing to declare exccept my genuis',
+    'The truth is rarely pure and never simple'
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text('School Card ID'),
+        title: Text('Awesome Quotes'),
         centerTitle: true,
-        backgroundColor: Colors.grey[850],
+        backgroundColor: Colors.redAccent,
         elevation: 0.0,
       ),
       body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/thumb.jpg'),
-                radius: 40.0,
-              ),
-            ),
-            Divider(height: 60.0, color: Colors.grey[800]),
-            Text(
-              'NAME',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              'Rhytonne Brite',
-              style: TextStyle(
-                color: Colors.amberAccent[200],
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Text(
-              'CURRENT NINJA LEVEL',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              '$ninjaLevel',
-              style: TextStyle(
-                color: Colors.amberAccent[200],
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.email,
-                  color: Colors.amberAccent[200],
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 0.0),
-                  child: Text(
-                    'hytonne@outlook.com',
-                    style: TextStyle(
-                        fontSize: 18.0,
-                        letterSpacing: 1.0,
-                        color: Colors.white),
-                  ),
-                )
-              ],
-            ),
-          ],
+          /* children: quotes.map((quote) {
+            return Text(quote);
+          }).toList(), */
+          children: quotes.map((quote) => Text(quote)).toList(),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            ninjaLevel += 1;
-          });
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.grey[700],
       ),
     );
   }
